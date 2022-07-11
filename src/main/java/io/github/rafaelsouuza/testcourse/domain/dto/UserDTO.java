258b81c85195e0d847e350d5bca74ed96494e0f4
Rafael Souza <snrafa.souza@gmail.com>
2022-07-11 12:44:35 -0300

    classe user dto

---
+++ src/main/java/io/github/rafaelsouuza/testcourse/domain/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package io.github.rafaelsouuza.testcourse.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
