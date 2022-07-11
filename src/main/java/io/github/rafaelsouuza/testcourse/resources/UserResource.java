56298bc4d90acbf1c5a82f3ac8ef209562e873f8
Rafael Souza <snrafa.souza@gmail.com>
2022-07-11 15:04:57 -0300

    excecao para email ja cadastrado na atualizacao

--- src/main/java/io/github/rafaelsouuza/testcourse/resources/UserResource.java
+++ src/main/java/io/github/rafaelsouuza/testcourse/resources/UserResource.java
@@ -1,16 +1,12 @@
 package io.github.rafaelsouuza.testcourse.resources;
 
+import io.github.rafaelsouuza.testcourse.domain.User;
 import io.github.rafaelsouuza.testcourse.domain.dto.UserDTO;
 import io.github.rafaelsouuza.testcourse.services.UserService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -44,5 +40,11 @@ public class UserResource {
                 .buildAndExpand(service.create(obj).getId())
                 .toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
+        obj.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
     }
 }
