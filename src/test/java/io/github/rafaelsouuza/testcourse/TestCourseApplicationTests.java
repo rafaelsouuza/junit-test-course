839f05db56802a29c81fe18d1ad359f7d7a9fc36
Rafael Souza <snrafa.souza@gmail.com>
2022-07-11 10:59:40 -0300

    Projeto iniciado

---
+++ src/test/java/io/github/rafaelsouuza/testcourse/TestCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rafaelsouuza.testcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
