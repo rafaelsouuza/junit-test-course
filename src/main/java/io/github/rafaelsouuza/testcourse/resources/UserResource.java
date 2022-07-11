88139d0381c1a9ce88465ac3f1fd8675ba207eb7
Rafael Souza <snrafa.souza@gmail.com>
2022-07-11 11:58:27 -0300

    classe user resource findById

--- src/main/java/io/github/rafaelsouuza/testcourse/resources/UserResource.java
+++ src/main/java/io/github/rafaelsouuza/testcourse/resources/UserResource.java
@@ -1,6 +1,8 @@
 package io.github.rafaelsouuza.testcourse.resources;
 
 import io.github.rafaelsouuza.testcourse.domain.User;
+import io.github.rafaelsouuza.testcourse.services.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,10 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
+@AllArgsConstructor
 public class UserResource {
+
+    private UserService service;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Rafael", "rafael@email.com", "12345"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
